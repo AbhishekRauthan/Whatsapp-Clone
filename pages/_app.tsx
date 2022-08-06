f13a6ee5c7cab778a3596972b6ce659093f7e0a1
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-08-06 16:03:25 +0530

    The following was done:
    	- installed react firebase hooks.
    	- added logic to redirect if not signed up.
    	- added logic to show loading spinner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Loading from "../components/Loading";
+import { auth } from "../utils/firebase";
+import Login from "./login";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) return <Loading />;
+  if (!user) return <Login />;
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
