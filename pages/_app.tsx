981aa86610ffaad53ac560e6985b81d58682fe89
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-08-09 18:52:08 +0530

    Added Logic to capture and add user to firestore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Loading from "../components/Loading";
-import { auth } from "../utils/firebase";
+import { auth, db } from "../utils/firebase";
 import Login from "./login";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      const capUsr = async () => {
+        await setDoc(
+          doc(db, "users", user.uid),
+          {
+            email: user.email,
+            lastSeen: serverTimestamp(),
+            photoUrl: user.photoURL,
+          },
+          { merge: true }
+        );
+      };
+      capUsr();
+    }
+  }, [user]);
 
   if (loading) return <Loading />;
   if (!user) return <Login />;
